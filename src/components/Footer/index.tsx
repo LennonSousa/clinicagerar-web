9394303f7a487c88e5819a26802942fd29392c51
lennonsousa <lennonsousa@outlook.com>
2021-03-03 18:20:28 -0300

    Page header e footer.

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,18 +1,79 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { Container, Row, Col, Image } from 'react-bootstrap';
+import { FaWhatsapp, FaInstagram, FaFacebookSquare } from 'react-icons/fa';
 
 import './styles.css';
-//import logoImg from '../../assets/images/logo-horizontal-branco.svg';
+import logoImg from '../../assets/images/logo-horizontal-branco.svg';
 import footerBackground from '../../assets/images/footer-background.svg';
 
 function Footer() {
     return (
         <footer style={{ backgroundImage: `url(${footerBackground})` }}>
-            <Container>
-                <Row>
-                    <Col>
-
+            <Container className="footer-container">
+                <Row className="footer-row justify-content-center align-items-center pt-5">
+                    <Col className="col-6" sm={2}>
+                        <Image fluid src={logoImg} alt="Logomarca Clínica Gerar" />
                     </Col>
+
+                    <Col className="mt-3 bt-3" sm={10}>
+                        <Row>
+                            <Col className="mt-3 bt-3" sm={4}>
+                                <Row>
+                                    <Col>
+                                        <h5 className="footer-section-title">Redes sociais</h5>
+                                    </Col>
+                                </Row>
+
+                                <Row>
+                                    <Col>
+                                        <Link className="footer-section-text" to="/"><h6><FaInstagram size={24} /> @clinicagerar</h6></Link>
+                                        <Link className="footer-section-text" to="/"><h6><FaFacebookSquare size={24} /> Clinica Gerar Imperatriz</h6></Link>
+                                        <Link className="footer-section-text" to="/"><h6><FaWhatsapp size={24} /> 99 98798-4898</h6></Link>
+                                    </Col>
+                                </Row>
+                            </Col>
+
+                            <Col className="mt-3 bt-3" sm={4}>
+                                <Row>
+                                    <Col>
+                                        <h5 className="footer-section-title">Serviços</h5>
+                                    </Col>
+                                </Row>
+
+                                <Row>
+                                    <Col>
+                                        <Link className="footer-section-text" to="/"><h6>Ginecologia</h6></Link>
+                                        <Link className="footer-section-text" to="/"><h6>Obstetrícia</h6></Link>
+                                        <Link className="footer-section-text" to="/"><h6>Ultrassonografia</h6></Link>
+                                        <Link className="footer-section-text" to="/"><h6>Medicina fetal</h6></Link>
+                                        <Link className="footer-section-text" to="/"><h6>Cirurgia plástica</h6></Link>
+                                    </Col>
+                                </Row>
+                            </Col>
+
+                            <Col className="mt-3 bt-3" sm={4}>
+                                <Row>
+                                    <Col>
+                                        <h5 className="footer-section-title">Endereço</h5>
+                                    </Col>
+                                </Row>
+
+                                <Row>
+                                    <Col>
+                                        <div className="footer-section-text"><h6>Av. Bernardo Sayão, 3650</h6></div>
+                                        <div className="footer-section-text"><h6>Medical Center, 5º andar</h6></div>
+                                        <div className="footer-section-text"><h6>Bairro Três Poderes</h6></div>
+                                        <div className="footer-section-text"><h6>Imperatriz - MA</h6></div>
+                                        <div className="footer-section-text"><h6>65698-000</h6></div>
+                                    </Col>
+                                </Row>
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+                <Row>
+
                 </Row>
             </Container>
         </footer>
--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -1,12 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { Container, Navbar, Nav } from 'react-bootstrap';
+import { Container, Navbar, Nav, Row, Col, Modal, Button, Image } from 'react-bootstrap';
+import { FaWhatsapp, FaRegCalendarAlt } from 'react-icons/fa';
 
 import './styles.css';
 import logoImg from '../../assets/images/logo-horizontal-branco.svg';
+import scheduleImage from '../../assets/images/undraw_Work_time_re_hdyv.svg';
 
 function PageHeader() {
     const [show, setShow] = useState(true);
+
+    const [showModalSchedule, setShowModalSchedule] = useState(false);
+
+    const handleClose = () => setShowModalSchedule(false);
+    const handleShow = () => setShowModalSchedule(true);
 
     const navbarControl = () => {
         if (window.scrollY > 300) {
@@ -24,7 +31,7 @@ function PageHeader() {
     return (
         <>
             <Navbar className="nav-container-top" variant="dark" expand="lg">
-                <Container>
+                <Container className="nav-top-container">
                     <Navbar.Brand href="#home">
                         <img
                             src={logoImg}
@@ -33,6 +40,46 @@ function PageHeader() {
                             alt="Clínica Gerar"
                         />
                     </Navbar.Brand>
+
+                    <Navbar.Collapse className="justify-content-end">
+                        <Nav>
+                            <Row>
+                                <Col>
+                                    <Row>
+                                        <Col className="nav-contact-container-link">
+                                            <Nav.Link href="https://api.whatsapp.com/send?phone=+5599991091718" target="_blank"><FaWhatsapp size={24} /> {` `} 99 99109-1718</Nav.Link>
+                                        </Col>
+                                    </Row>
+                                    <Row>
+                                        <Col className="nav-contact-container-text">
+                                            Dr. Evaldo Reis Silva
+                                        </Col>
+                                    </Row>
+                                </Col>
+
+                                <Col>
+                                    <Row>
+                                        <Col className="nav-contact-container-link">
+                                            <Nav.Link href="https://api.whatsapp.com/send?phone=+5599988090998" target="_blank"><FaWhatsapp size={24} /> {` `} 99 98809-0998</Nav.Link>
+                                        </Col>
+                                    </Row>
+                                    <Row>
+                                        <Col className="nav-contact-container-text">
+                                            Dra. Cristina Célia Andrade
+                                        </Col>
+                                    </Row>
+                                </Col>
+
+                                <Col>
+                                    <Row>
+                                        <Col className="nav-contact-container-link">
+                                            <Nav.Link onClick={handleShow} ><FaRegCalendarAlt size={24} /> {` `} Horário de atendimento</Nav.Link>
+                                        </Col>
+                                    </Row>
+                                </Col>
+                            </Row>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
 
@@ -46,26 +93,106 @@ function PageHeader() {
                 sticky="top"
             >
                 <Container>
-                    <Navbar.Brand href="#home">
+                    <Navbar.Brand href="#home" className="nav-brand-bottom" style={{ top: show ? "-100%" : 10 }}>
                         <img
                             style={{ opacity: show ? 0 : 1 }}
                             src={logoImg}
                             height="30"
-                            className="align-top nav-brand-bottom"
+                            className="align-top"
                             alt="Clínica Gerar"
                         />
                     </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+                    <div className="nav-bottom-container">
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    </div>
+
                     <Navbar.Collapse className="justify-content-center" id="basic-navbar-nav">
                         <Nav className="mr-auto">
                             <Link to="/" className="nav-link nav-header-link-active">INÍCIO</Link>
                             <Link to="/" className="nav-link nav-header-link">SOBRE NÓS</Link>
                             <Link to="/" className="nav-link nav-header-link">AGENDAMENTO</Link>
                             <Link to="/" className="nav-link nav-header-link">CONTATO</Link>
+
+                            <div className="nav-bottom-links-container">
+                                <Row>
+                                    <Col className="mt-3" sm={4}>
+                                        <Row>
+                                            <Col className="nav-contact-container-link">
+                                                <Nav.Link href="https://api.whatsapp.com/send?phone=+5599991091718" target="_blank"><FaWhatsapp size={24} /> {` `} 99 99109-1718</Nav.Link>
+                                            </Col>
+                                        </Row>
+                                        <Row>
+                                            <Col className="nav-contact-container-text">
+                                                Dr. Evaldo Reis Silva
+                                        </Col>
+                                        </Row>
+                                    </Col>
+
+                                    <Col className="mt-3" sm={4}>
+                                        <Row>
+                                            <Col className="nav-contact-container-link">
+                                                <Nav.Link href="https://api.whatsapp.com/send?phone=+5599988090998" target="_blank"><FaWhatsapp size={24} /> {` `} 99 98809-0998</Nav.Link>
+                                            </Col>
+                                        </Row>
+                                        <Row>
+                                            <Col className="nav-contact-container-text">
+                                                Dra. Cristina Célia Andrade
+                                        </Col>
+                                        </Row>
+                                    </Col>
+
+                                    <Col className="mt-3" sm={4}>
+                                        <Row>
+                                            <Col className="nav-contact-container-link">
+                                                <Nav.Link onClick={handleShow} ><FaRegCalendarAlt size={24} /> {` `} Horário de atendimento</Nav.Link>
+                                            </Col>
+                                        </Row>
+                                    </Col>
+                                </Row>
+                            </div>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
+
+            <Modal show={showModalSchedule} onHide={handleClose}>
+                <Modal.Header>
+                    <Modal.Title style={{ color: "#003A3C", fontWeight: 600 }}>Nossos horários de funcionamento</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Row className="mt-4 mb-4 justify-content-center align-items-center text-center">
+                        <Col sm={5}>
+                            <Image fluid src={scheduleImage} alt="Horário de funcionamento" />
+                        </Col>
+
+                        <Col sm={5}>
+                            <Row className="mb-2">
+                                <Col>
+                                    <h5 style={{ color: "#003A3C", fontWeight: 600 }}>De segunda a sexta.</h5>
+                                </Col>
+                            </Row>
+
+                            <Row>
+                                <Col>
+                                    <h6>Das 08h às 12h</h6>
+                                </Col>
+                            </Row>
+
+                            <Row>
+                                <Col>
+                                    <h6>e das 14h às 18h.</h6>
+                                </Col>
+                            </Row>
+                        </Col>
+                    </Row>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Fechar
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </>
     );
 }
